a72c86904c320005bdf652fd61b1bf66f28aaaf4
MacCloud <kuzabala@my.cspc.edu.ph>
2025-04-23 11:10:26 +0800

    remodeled

--- FInals/LT 2/BSIT2H_Zabala.java
+++ FInals/LT 2/BSIT2H_Zabala.java
@@ -17,16 +17,16 @@ public class BSIT2H_Zabala {
 
             // 1. Get all available menu items (across all menus)
             System.out.println("Available Menu Items:");
-            NodeList availableItems = (NodeList) xPath.evaluate("//item[@available='true']/name", doc, XPathConstants.NODESET);
-            for (int i = 0; i < availableItems.getLength(); i++) {
-                System.out.println(" - " + availableItems.item(i).getTextContent());
+            NodeList no1 = (NodeList) xPath.evaluate("//item[@available='true']/name", doc, XPathConstants.NODESET);
+            for (int i = 0; i < no1.getLength(); i++) {
+                System.out.println(" - " + no1.item(i).getTextContent());
             }
 
             // 2. Retrieve all Pizza items and their prices
             System.out.println("\nPizza Items and Prices:");
-            NodeList pizzaItems = (NodeList) xPath.evaluate("//item[category='Pizza']", doc, XPathConstants.NODESET);
-            for (int i = 0; i < pizzaItems.getLength(); i++) {
-                Element item = (Element) pizzaItems.item(i);
+            NodeList no2 = (NodeList) xPath.evaluate("//item[category='Pizza']", doc, XPathConstants.NODESET);
+            for (int i = 0; i < no2.getLength(); i++) {
+                Element item = (Element) no2.item(i);
                 String name = item.getElementsByTagName("name").item(0).getTextContent();
                 String price = item.getElementsByTagName("price").item(0).getTextContent();
                 System.out.println(" - " + name + ": $" + price);
@@ -34,23 +34,23 @@ public class BSIT2H_Zabala {
 
             // 3. Get names of all dine-in menu items
             System.out.println("\nDine-in Menu Items:");
-            NodeList dineInItems = (NodeList) xPath.evaluate("//menu[@type='dine-in']/item/name", doc, XPathConstants.NODESET);
-            for (int i = 0; i < dineInItems.getLength(); i++) {
-                System.out.println(" - " + dineInItems.item(i).getTextContent());
+            NodeList no3 = (NodeList) xPath.evaluate("//menu[@type='dine-in']/item/name", doc, XPathConstants.NODESET);
+            for (int i = 0; i < no3.getLength(); i++) {
+                System.out.println(" - " + no3.item(i).getTextContent());
             }
 
             // 4. List ingredients of the Cheeseburger
             System.out.println("\nIngredients of Cheeseburger:");
-            NodeList cheeseburgerIngredients = (NodeList) xPath.evaluate("//item[name='Cheeseburger']/ingredients/ingredient", doc, XPathConstants.NODESET);
-            for (int i = 0; i < cheeseburgerIngredients.getLength(); i++) {
-                System.out.println(" - " + cheeseburgerIngredients.item(i).getTextContent());
+            NodeList no4 = (NodeList) xPath.evaluate("//item[name='Cheeseburger']/ingredients/ingredient", doc, XPathConstants.NODESET);
+            for (int i = 0; i < no4.getLength(); i++) {
+                System.out.println(" - " + no4.item(i).getTextContent());
             }
 
             // 5. Find the price and currency of Grilled Cheese
             System.out.println("\nPrice and Currency of Grilled Cheese:");
-            String grilledCheesePrice = xPath.evaluate("//item[name='Grilled Cheese']/price", doc);
-            String grilledCheeseCurrency = xPath.evaluate("//item[name='Grilled Cheese']/price/@currency", doc);
-            System.out.println(" - Price: " + grilledCheesePrice + " " + grilledCheeseCurrency);
+            String no5Price = xPath.evaluate("//item[name='Grilled Cheese']/price", doc);
+            String no5Currency = xPath.evaluate("//item[name='Grilled Cheese']/price/@currency", doc);
+            System.out.println(" - Price: " + no5Price + " " + no5Currency);
 
         } catch (IOException | ParserConfigurationException | XPathExpressionException | DOMException | SAXException e) {
             System.err.println("Error: " + e.getMessage());
